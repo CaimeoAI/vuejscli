6b6b51a70318dc336512046782d2eecdf0583b41
Judielle Villa <judiellezinke@yahoo.de>
2022-07-08 22:41:15 +0200

    cleaned boilerplate and added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>Task Tracker</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
+
+<style scoped>
+    header {}
+</style>
